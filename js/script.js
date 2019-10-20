c96c316c809416e76b02bfc56fb6947d3cf8eda5
shubham_9195 <shubham@servitor.in>
2019-10-20 19:17:04 +0530

    Filtering Header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-fetch('https://newsapi.org/v2/everything?q=reactjs&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1')
-.then(response=> response.json())
-    .then(myJson=> console.log(myJson))
+function search(){
+    var query = document.getElementById('searchBar').value
+    fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1`)
+    .then(response=> response.json())
+        .then(myJson=> console.log(myJson))
+    
+}
