d8ebc55cf86d3f5f4352d9fa7e95e3b3f65d4010
shubham_9195 <shubham@servitor.in>
2019-10-20 20:56:27 +0530

    data iteration done image data

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-function search(){
+function search() {
     var query = document.getElementById('searchBar').value
+    var container = document.getElementById('imgList');
     fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1`)
-    .then(response=> response.json())
-        .then(myJson=> 
-        
-        // iterating data
-        myJson.articles.map((item, index) => {
-            // appending description
-            document.getElementById("demo").innerHTML += index + ":" + item.description + "<br>"; 
-        })
+        .then(response => response.json())
+        .then((myJson) =>
+
+            // iterating data
+
+            myJson.articles.map((item, index) => {
+                document.getElementById("demo").innerHTML += item.description + "<br>";
+                if (item.urlToImage !== null) {
+                    var imageSpan = document.createElement('span')
+                    var img = document.createElement('img');
+                    var linebreak = document.createElement("br");
+                    img.src = item.urlToImage;
+                    img.width = 100
+                    img.height = 50
+                    imageSpan.appendChild(img);
+
+                    container.appendChild(imageSpan);
+                    console.log('imagechk', item.urlToImage);
+                }
+            })
         )
 }
