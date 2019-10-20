ff59106f44e087e0dd8514be2cc5f5c98862b9f3
shubham_9195 <shubham@servitor.in>
2019-10-20 19:36:55 +0530

    data iteration done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ function search(){
     var query = document.getElementById('searchBar').value
     fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=1`)
     .then(response=> response.json())
-        .then(myJson=> console.log(myJson))
-    
+        .then(myJson=> 
+        
+        // iterating data
+        myJson.articles.map((item, index) => {
+            // appending description
+            document.getElementById("demo").innerHTML += index + ":" + item.description + "<br>"; 
+        })
+        )
 }
