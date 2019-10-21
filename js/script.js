1917fe83e34f217d69db72a560fecae424da97ad
shubham_9195 <shubham@servitor.in>
2019-10-21 22:39:27 +0530

    data iteration done image aligned

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+var timeLeft = 30;
+var elem = document.getElementById('timer');
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+        refresh();
+    } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+    }
+}
+
+function refresh() {
+    window.location.reload(1)
+}
+
 function search() {
     var query = document.getElementById('searchBar').value
     var container = document.getElementById('imgList');
@@ -6,9 +24,8 @@ function search() {
         .then((myJson) =>
 
             // iterating data
-
-            myJson.articles.map((item, index) => {
-                document.getElementById("demo").innerHTML += item.description + "<br>";
+            myJson.articles.slice(0,9).map((item, index) => {
+                document.getElementById("demo").innerHTML += item.title + "<br>";
                 if (item.urlToImage !== null) {
                     var imageSpan = document.createElement('span')
                     var img = document.createElement('img');
@@ -17,10 +34,21 @@ function search() {
                     img.width = 100
                     img.height = 50
                     imageSpan.appendChild(img);
-
                     container.appendChild(imageSpan);
                     console.log('imagechk', item.urlToImage);
                 }
+                else {
+
+                    var img = document.createElement('img');
+                    img.src ='http://static1.squarespace.com/static/582f2a75414fb5c5d7172211/t/5ba29eacb8a045e82a32378a/1537384155646/blank-thumbnail.jpg?format=1500w'
+;
+                    img.width = 100
+                    img.height = 50
+                    container.appendChild(img);
+                    console.log('imagechk','https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
+);
+                }
             })
         )
+        
 }
